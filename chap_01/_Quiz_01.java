9920e42d6c393c3608c28e4d98c6c7f885a6ab9c
sjun <sammanone@naver.com>
2023-08-29 15:35:57 +0900

    0829

--- chap_01/_Quiz_01.java
+++ chap_01/_Quiz_01.java
@@ -5,32 +5,20 @@ package chap_01;
 약 3분 후 도착
 남은 거리 1.2km
  */
-
 public class _Quiz_01 {
     public static void main(String[] args) {
-        String busNum = "상암08";
-        int min = 3;
+        // 버스 번호 (1234, 상암08)
+        String busNo = "상암08";
+
+        // 남은 시간 (3분, 5분)
+        int minute = 3;
+
+        // 남은 거리 (1.5km, 0.8km)
         double distance = 1.2;
-        System.out.println(busNum + "번 버스");
-        System.out.println("약 " + min + "분 후 도착");
+
+        // 결과 출력
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
         System.out.println("남은 거리 " + distance + "km");
     }
 }
-
-//public class _Quiz_01 {
-//    public static void main(String[] args) {
-//        // 버스 번호 (1234, 상암08)
-//        String busNo = "상암08";
-//
-//        // 남은 시간 (3분, 5분)
-//        int minute = 3;
-//
-//        // 남은 거리 (1.5km, 0.8km)
-//        double distance = 1.2;
-//
-//        // 결과 출력
-//        System.out.println(busNo + "번 버스");
-//        System.out.println("약 " + minute + "분 후 도착");
-//        System.out.println("남은 거리 " + distance + "km");
-//    }
-//}
